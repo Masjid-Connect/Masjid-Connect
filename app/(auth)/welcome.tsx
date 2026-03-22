ee99efceb85a88e9bbb335b4ed1f4a36751f97ea
Abu Aaliyah <info@eazyaccess.org>
2026-03-22 06:21:56 +0000

    Disable PKCE in authentication request

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -236,6 +236,7 @@ export default function WelcomeScreen() {
         redirectUri,
         scopes: ['openid', 'profile', 'email'],
         responseType: AuthSession.ResponseType.IdToken,
+        usePKCE: false,
         extraParams: { nonce },
       });
 
